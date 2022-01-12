9a67a3db3e44a8d347e6715f7c884071336fcf0f
unknown <nelberthsanchezomdb@gmail.com>
2022-01-12 13:21:48 -0400

    Se actualizo el header

--- src/app/controlador/controlador.component.ts
+++ src/app/controlador/controlador.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ControladorComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  
+   }
 
   ngOnInit(): void {
+    var monitoreo=document.getElementById('monitoreo') as HTMLLIElement;
+    monitoreo.addEventListener('click', (e)=>{
+      var hola=e.target as Element;      
+      var padre = hola.parentElement as HTMLLIElement;
+      var abuelo = padre.parentElement as HTMLLIElement;
+      var holo = monitoreo.querySelectorAll(".activado");
+      holo.forEach((e)=>{
+        e.classList.remove('activado')
+      })
+      if(hola && hola.tagName=== 'LI'){
+        if(abuelo.className==='despegable'){
+            abuelo.classList.add('activado')
+        }
+            hola.classList.add('activado')
+        
+      }    
+    })
   }
+
+
+
+
+
 
 }

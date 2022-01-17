cde50f83672a183c2d15ae5b7185a611311f59c5
DevPrismaHexagon <josuempadroncmdb@gmail.com>
2022-01-17 12:15:14 -0400

    actualiuzacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { ControladorComponent } from './controlador/controlador.component';
 import { LoginComponent } from './login/login.component';
-import { RegistroComponent } from './registro/registro.component';
+// import { RegistroComponent } from './registro/registro.component';
 import { IniciarSessionComponent } from './iniciar-session/iniciar-session.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { ComprasComponent } from './compras/compras.component';
+
 
 const rutas:Routes=[
+  {path:'compras',component:ComprasComponent,
+
+  },
+
   {path:'',component:ControladorComponent},
   {path:'login',
     component:LoginComponent,
     children:[
-      {path:'iniciar',component:IniciarSessionComponent},
-      {path:'registro',component:RegistroComponent}
-    ]  
-  },
+      {path:'iniciar',component:IniciarSessionComponent}
+    //   {path:'registro',component:RegistroComponent}
+     ]  
+  }
 
 ]
 
@@ -28,8 +34,9 @@ const rutas:Routes=[
     AppComponent,
     ControladorComponent,
     LoginComponent,
-    RegistroComponent,
-    IniciarSessionComponent
+    // RegistroComponent,
+    IniciarSessionComponent,
+    ComprasComponent
   ],
   imports: [
     BrowserModule, 
--- src/app/compras/compras.component.spec.ts
+++ src/app/compras/compras.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RegistroComponent } from './registro.component';
+import { ComprasComponent } from './compras.component';
 
-describe('RegistroComponent', () => {
-  let component: RegistroComponent;
-  let fixture: ComponentFixture<RegistroComponent>;
+describe('ComprasComponent', () => {
+  let component: ComprasComponent;
+  let fixture: ComponentFixture<ComprasComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegistroComponent ]
+      declarations: [ ComprasComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(RegistroComponent);
+    fixture = TestBed.createComponent(ComprasComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
